8d4d4c66ca984749740e02e71aeec10c581da512
Kevin Nunes <contact@nvkdesign.fr>
2016-01-09 23:29:47 +0100

    sessions & routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
+var session = require('cookie-session'); // middleware session
+var bodyParser = require('body-parser'); // middleware gestion de paramètres
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 var express = require('express');
 var router = express.Router();
 
+router.use(session({secret: 'todo'}))
+
+
+.use(function(req, res, next){
+  if (typeof (req.session.todolist) == 'undefined') {
+    req.session.todolist = [];
+  }
+  next();
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Votre Todolist !' });
+  res.render('index', {
+    title: 'Votre Todolist !',
+    todolist: req.session.todolist
+  });
+});
+
+router.post('/ajouter/', urlencodedParser, function(req, res){
+  if (req.body.newtodo != '') {
+    req.session.todolist.push(req.body.newtodo);
+  }
+  res.redirect('/');
+});
+
+router.get('/supprimer/:id', function(res, req){
+  if (req.params.id != '') {
+    req.session.todolist.splice(req.params.id, 1);
+  }
+  res.redirect('/');
+})
+
+.use(function (req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
