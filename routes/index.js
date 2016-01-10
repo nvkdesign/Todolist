fbcee0f82dacd0f0731c3f74d276e74e963c6a44
Kevin Nunes <contact@nvkdesign.fr>
2016-01-10 22:54:29 +0100

    fix todolist & index.js

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
+var express = require('express');
+
 var session = require('cookie-session'); // middleware session
 var bodyParser = require('body-parser'); // middleware gestion de paramètres
 var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-var express = require('express');
 var router = express.Router();
+router.use(session({secret: 'todo'}));
 
-router.use(session({secret: 'todo'}))
 
-
-.use(function(req, res, next){
+router.use(function(req, res, next){
   if (typeof (req.session.todolist) == 'undefined') {
     req.session.todolist = [];
   }
   next();
-});
+})
 
 
 /* GET home page. */
-router.get('/todo', function(req, res, next) {
+.get('/todo', function(req, res, next) {
   res.render('index.jade', {
     title: 'Votre Todolist !',
     todolist: req.session.todolist
   });
-});
+})
 
-router.post('/todo/ajouter/', urlencodedParser, function(req, res){
+.post('/todo/ajouter/', urlencodedParser, function(req, res){
   if (req.body.newtodo != '') {
     req.session.todolist.push(req.body.newtodo);
   }
   res.redirect('/todo');
 })
 
-.get('/todo/supprimer/:id', function(res, req){
+.get('/todo/supprimer/:id', function(req, res) {
   if (req.params.id != '') {
     req.session.todolist.splice(req.params.id, 1);
   }
